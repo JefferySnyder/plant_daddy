75eadcfa6bbddf5c07a8a145687a8b63fe6b4e85
JefferySnyder <jayasnyd@gmail.com>
2025-03-02 13:47:21 -0700

    Add RepeatedItem class

---
+++ RepeatedItem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    class RepeatedItem
+    {
+        public Texture2D texture;
+        public List<AnimatedTexture> items;
+
+        public void Initialize()
+        {
+            items = new List<AnimatedTexture>();
+        }
+        public void Load(ContentManager content, string asset)
+        {
+            texture = content.Load<Texture2D>(asset);
+        }
+        public void Update(GameTime gameTime)
+        {
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var i in items)
+            {
+                i.DrawFrame(spriteBatch);
+            }
+        }
+    }
+}
